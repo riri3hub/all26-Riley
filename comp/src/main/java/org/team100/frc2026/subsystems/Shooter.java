e1eb7bedc730e3a9acbd6305fe77803dc5379da1
marcelosin1010 <marcelo.sin1010@gmail.com>
2026-03-28 11:31:18 -0700

    more changes

--- comp/src/main/java/org/team100/frc2026/subsystems/Shooter.java
+++ comp/src/main/java/org/team100/frc2026/subsystems/Shooter.java
@@ -38,8 +38,7 @@ public class Shooter extends SubsystemBase {
     private static final double GEAR_RATIO = 1;
     private static final double WHEEL_DIAMETER_M = .115;
 
-    /** Speed used in selftest. */
-    private static final double FULL_SPEED = 20;
+    private static final double FULL_SPEED = 30;
 
     private final Supplier<OptionalDouble> m_speed;
 
